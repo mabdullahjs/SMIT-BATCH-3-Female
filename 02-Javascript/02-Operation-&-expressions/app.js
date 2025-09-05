7a7032b460366351796e76211c18152b29673719
mabdullahjs <mabdullah2037@outlook.com>
2025-09-05 17:58:41 +0500

    javascript started

---
+++ 02-Javascript/02-Operation-&-expressions/app.js
@@ -0,0 +1,65 @@
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % == modulus
+
+
+// var num1 = 10;
+// var num2 = 20;
+
+// console.log(num1 + (num2 / num1));
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num2 % num1);
+
+
+
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+
+// var num1 = 10
+// num1 = num1 + 20
+// num1 += 20
+// console.log(num1);
+
+
+
+var num1 = 10;
+// num1 -= 5 // num1 = num1 - 5
+
+// num1 *= 5
+num1 /= 5
+console.log(num1);
+
+
+
+
+//Comparision Operator
+
+// >
+// <
+// == || !=
+// === || !==
+// != 
+// !== 
+// >=
+// <=
+
+
+//logical Operator
+
+//&& == and
+//|| == OR

a64074820bcb1120f879f6b5431e6e6a3ee22e7f
mabdullahjs <mabdullah2037@outlook.com>
2025-09-12 18:01:19 +0500

    if else

---
+++ 02-Javascript/03-Conditional-statement/app.js
@@ -0,0 +1,95 @@
+
+// block | scope
+// if (age > 18){
+//     console.log("welcome to gym");
+    
+// } else {
+//     console.log("app chota ho abhi");
+    
+// }
+
+
+
+// var age = 12;
+
+// if(age >= 18){
+//     console.log("welcome to gym");
+    
+// } else if (age >= 15){
+//     console.log("1000 zyada do or ajao");
+    
+// } else {
+//     console.log("dafa hojao");
+    
+// }
+
+
+
+// var speed = 80;
+
+// if (speed < 30){
+//     console.log("Too slow");
+// } else if (speed >= 30 && speed <= 60){
+//     console.log("Safe speed");
+    
+// } else if(speed >= 61 && speed <= 100){
+//     console.log("Speeding");
+    
+// } else {
+//     console.log("Dangerously fast");
+    
+// }
+
+
+
+// var days = 3;
+
+
+// if(days === 0){
+//     console.log("0$ fine");
+// } else if(days <= 5) {
+//     console.log("1$ fine" , "total fine ==> " + 1 * days);
+    
+// } else if(days <= 10) {
+//     console.log("2$ fine" , "total fine ==> " + 2 * days);
+    
+// } else {
+//     console.log("$5 fine" , "total fine ==> $" + 5 * days);
+    
+// }
+
+
+
+// var age = 21;
+// var ticket = false;
+
+
+// if(age >= 18 && ticket){
+//     console.log("welcome to our event");
+    
+// } else {
+//     console.log("kisna bulaya ha appko?");
+    
+// }
+
+
+
+// nested if else
+
+
+var age = 12;
+
+if(age > 15) {
+    console.log("greater then 15");
+    
+    if(age > 18) {
+        console.log("15 or 18 dono s bara ha");
+        
+    } else {
+        console.log("18 sa chota ha");
+        
+    }
+} else {
+    console.log("tum chota ho");
+    
+}

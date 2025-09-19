fdbcb71a31b864a4e51d3a8b17647ab1d165af3b
mabdullahjs <mabdullah2037@outlook.com>
2025-09-19 17:29:24 +0500

    functions

--- 02-Javascript/03-Conditional-statement/app.js
+++ 02-Javascript/03-Conditional-statement/app.js
@@ -79,17 +79,62 @@
 
 var age = 12;
 
-if(age > 15) {
-    console.log("greater then 15");
+// if(age > 15) {
+//     console.log("greater then 15");
     
-    if(age > 18) {
-        console.log("15 or 18 dono s bara ha");
+//     if(age > 18) {
+//         console.log("15 or 18 dono s bara ha");
         
-    } else {
-        console.log("18 sa chota ha");
+//     } else {
+//         console.log("18 sa chota ha");
         
-    }
+//     }
+// } else {
+//     console.log("tum chota ho");
+    
+// }
+
+
+
+// age 20 < student 21 - 60 (other) 60+ senior
+
+// var age = 11
+
+// if(age <= 20) {
+//     console.log("student 20%");
+    
+// } else if (age > 60) {
+//     console.log("senior 50%");
+// } else {
+//     console.log("other 0%");
+// }
+
+// 10. Meal Type Identifier
+// Based on time of day (0–23), identify meal:
+
+// 6–10: Breakfast
+// 12–14: Lunch
+// 18–21: Dinner
+// Any other time: Snack
+
+
+var time = 8
+
+if(time >= 6 && time <= 10){
+    console.log("breakfast");
+} else if(time >= 12 && time <= 14){
+    console.log("Lunch");
+} else if(time >= 18 && time <= 21){
+    console.log("Dinner");
 } else {
-    console.log("tum chota ho");
+    console.log("Snack");
     
 }
+
+
+
+
+
+
+
+

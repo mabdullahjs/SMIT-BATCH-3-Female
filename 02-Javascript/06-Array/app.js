76a2fcdce5812c9f8420f4ec3f0c1e41000a831b
mabdullahjs <mabdullah2037@outlook.com>
2025-10-03 17:02:55 +0500

    array

--- 02-Javascript/06-Array/app.js
+++ 02-Javascript/06-Array/app.js
@@ -1,6 +1,5 @@
 // console.log("Hello world");
 
-
 // let username = "Muhammad Abdullah"
 
 // length
@@ -11,25 +10,7 @@
 // console.log(fruits[3]);
 // console.log(fruits[0]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// prototype method
 
 //push
 //pop
@@ -44,27 +25,75 @@
 //reverse
 //indexof
 
-
-
-
 // let mixedArr = ["apple" , 20 , true]
 // let nestedArr = [1 , [2 , [3 , [4 , [5]]]]]
 // console.log(nestedArr[1][1][1][1][0]);
 
+// let fruits = ["apple" , "banana" , "mango" , "grapes" , "watermelon" , "pineapple"];
+
+// // fruits.push("new item")
+// // fruits.pop()
+// // fruits.unshift("new item")
+// fruits.shift()
+// console.log(fruits);
 
 let fruits = ["apple" , "banana" , "mango" , "grapes" , "watermelon" , "pineapple"];
 
+// index 0
+// length 1
+// console.log(fruits[2]);
 
-// fruits.push("new item")
+// fruits.push("new fruit")
 // fruits.pop()
-// fruits.unshift("new item")
-fruits.shift()
-console.log(fruits);
+// fruits.shift()
+// fruits.unshift("updated")
+
+// fruits.splice(2 , 2 , "updated" , "updated two")
+// fruits.splice(2 , 0 , "updated")
+// fruits.splice(4 , 1)
+// console.log(fruits);
+
+// shallow copy bnata ha
+// let newFruits = fruits.slice(1 , 4)
+// console.log(newFruits);
+
+// let num1 = [1 , 2 , 3];
+// let num2 = [4 , 5 , 6];
+
+// let merged = num2.concat(num1)
+// console.log(merged);
+
+//sort
+
+let num1 = [6, 2, 8, 7, 1, 3, 9, 0];
+// console.log(num1.sort());
+// console.log(fruits.sort());
+
+//reverse
+// console.log(num1.reverse());
+// console.log(num1.sort().reverse());
+
+
+//indexof agar item mujood ha to uska index warna -1
+// console.log(num1.indexOf(72));
+// console.log(fruits.indexOf("melon"));
+
+// includes
+
+// console.log(num1.includes(72));
+// console.log(fruits.includes("appleas"));
+
+
+// split 
+let username = "Muhammad Abdullah khan"
+// console.log(username.split()); // string to array
+// console.log(fruits.join("zz")); // array to string
+
+console.log(username.split("").reverse().join(""));
 
 
 
 
 
 
-
-
+// join

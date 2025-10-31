6a881f2cf6555d1a8829db4ff88f07fa5430d1a0
mabdullahjs <mabdullah2037@outlook.com>
2025-10-31 17:17:45 +0500

    todo app completed

--- 02-Javascript/08-Todo-App/app.js
+++ 02-Javascript/08-Todo-App/app.js
@@ -7,34 +7,32 @@
 // update
 // delete []
 
-
 const input = document.querySelector("#input");
 const ol = document.querySelector("ol");
 const allTodos = [];
 
-function addTodo(){
-    ol.innerHTML = ""
-    allTodos.push(input.value);
-
-    for(let i = 0; i < allTodos.length; i++){
-        ol.innerHTML += `<li>${allTodos[i]}
-        <button onclick="deleteTodo()">delete</button>
-        <button onclick="editTodo()">edit</button>
-        </li>`
-    }
-    
-
-
-
-    input.value = ""
+function renderTodo() {
+  ol.innerHTML = "";
+  for (let i = 0; i < allTodos.length; i++) {
+    ol.innerHTML += `<li>${allTodos[i]}
+        <button onclick="deleteTodo(${i})">delete</button>
+        <button onclick="editTodo(${i})">edit</button>
+        </li>`;
+  }
 }
 
-
-function deleteTodo(){
-    console.log("todo deleted");
-    
+function addTodo() {
+  allTodos.push(input.value);
+  renderTodo()
+  input.value = "";
 }
-function editTodo(){
-    console.log("todo edited");
-    
+
+function deleteTodo(index) {
+  allTodos.splice(index, 1);
+  renderTodo()
+}
+function editTodo(index) {
+    const updatedVal = prompt("enter updated val");
+    allTodos.splice(index , 1 , updatedVal);
+    renderTodo()
 }

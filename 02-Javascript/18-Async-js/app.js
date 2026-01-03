8a2ac774d15f391016392f74b4eeb09a207704eb
mabdullahjs <mabdullah2037@outlook.com>
2026-01-03 12:01:17 +0500

    async

--- 02-Javascript/18-Async-js/app.js
+++ 02-Javascript/18-Async-js/app.js
@@ -1,7 +1,6 @@
 // asynchronous
 // synchronous
 
-
 // console.log("1");
 // console.log("2");
 // console.log("3");
@@ -10,17 +9,11 @@
 // }, 0);
 // console.log("5");
 
-
-
 // let data = rukjaa dataMangwalia() // time taken
 // console.log(data)
 
-
-
 // single threaded language
 // aik hi computation perform krskti ha
-
-
 
 // set timeout
 // set interval
@@ -32,9 +25,44 @@
 
 // console.log("hello");
 
+let grade = "Adasd";
 
+// if(grade === "A+"){
+//     console.log("Best ha..");
 
+// } else if(grade === "A"){
+//     console.log("Good haa..");
+
+// } else if (grade === "B"){
+//     console.log("Mehnat kro...");
+
+// } else {
+//     console.log("Chalaang maarlo chat sa..");
+
+// }
+
+switch (grade) {
+  case "A+":
+    console.log("Best ha..");
+    break;
+  case "A":
+    console.log("Good haa..");
+    break;
+  case "B":
+    console.log("Mehnat kro...");
+    break;
+  default:
+    console.log("Chalaang maarlo chat sa..");
+    break;
+}
+
+// settimeout 2 console
 
 // promises
+
+// pending
+// fulfilled
+// rejected
+
 // async await
 // fetch
